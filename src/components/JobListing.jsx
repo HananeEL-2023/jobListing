9739e4ead9c9a8bb3e4164f18f01acebbd45b969
Hanane <hanane.elkaaba1@gmail.com>
2023-12-06 13:58:04 +0100

    Part two : Styling

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -7,18 +7,18 @@ export default function JobListing() {
       {data.map((job) => {
         return (
           <section key={job.id}>
-            <article className="flex items-center shadow w-9/12 mx-auto justify-between py-6">
+            <article className="flex items-center shadow w-9/12 mx-auto justify-between p-6">
               <div className="flex">
                 <figure className="mr-5">
-                  <img src={job.logo} alt="logo" />
+                  <img src={job.logo} alt="logo" className="w-3/4" />
                 </figure>
                 <div>
-                  <div className="flex justify-between">
-                    <h2 className="text-base text-desaturated-dark-cyan font-medium">
+                  <div className="flex mb-2 ">
+                    <h2 className="text-base text-desaturated-dark-cyan font-medium mr-1.5">
                       {job.company}
                     </h2>
                     {job.new && (
-                      <p className="text-base bg-desaturated-dark-cyan text-light-grayish-cyan2 p-1 rounded-b-2xl rounded-t-2xl py-0 px-2.5">
+                      <p className="text-base bg-desaturated-dark-cyan text-light-grayish-cyan2 p-1 rounded-b-2xl rounded-t-2xl py-0 px-2.5 mr-1.5">
                         New!
                       </p>
                     )}
@@ -28,13 +28,13 @@ export default function JobListing() {
                       </p>
                     )}
                   </div>
-                  <p className="font-bold text-base text-very-dark-grayish-cyan">
+                  <p className="font-bold text-base text-very-dark-grayish-cyan mb-2">
                     {job.position}
                   </p>
                   <ul className="flex text-base text-dark-grayish-cyan">
-                    <li>{job.postedAt}</li>
-                    <li>{job.contract}</li>
-                    <li>{job.location}</li>
+                    <li className="mr-5">{job.postedAt}</li>
+                    <li className="mr-5 list-disc">{job.contract}</li>
+                    <li className="list-disc">{job.location}</li>
                   </ul>
                 </div>
               </div>
